59a8120f3d78d041fdb2dfbf3c70fce30ec44b49
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-24 16:46:02 +0600

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,6 +1,6 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
