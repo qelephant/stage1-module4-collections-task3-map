2f0e8c36ffed17bbe9dbcb76cbf8929baf16fc08
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-24 16:53:36 +0600

    Update FunctionValueFinder.java

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -6,8 +6,17 @@ import java.util.Map;
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        return functionMap.containsValue(requiredValue);
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> functionMap = new HashMap<>();
+
+        for (int element : sourceList) {
+            int functionValue = 5 * element + 2;
+            functionMap.put(element, functionValue);
+        }
+
+        return functionMap;
     }
 }
